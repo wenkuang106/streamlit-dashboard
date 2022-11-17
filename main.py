4e89f67f7e97066dac33192054d094551825e386
Wenjin Kuang <wenkuajin@gmail.com>
2022-11-17 16:06:15 -0500

    Final

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ if st.checkbox('Show Chipotle East Dataset'):
 if st.checkbox('Show Chipotle West Dataset'):
     st.dataframe(chipotle_west)
 
-## df = pd.DataFrame(columns=['latitude', 'longitude'])
-st.map(chipotle_west)
-st.map(chipotle_east)
+if st.checkbox('Chipotle in Western America'):
+    st.subheader('This is shows the Chipotle areas in Western America')
+    st.map(chipotle_west)
+if st.checkbox('Chipotle in Eastern America'):
+    st.subheader('This is shows the Chipotle areas in Eastern America')
+    st.map(chipotle_east)
+
+## creating a bar and line graph of # of chipotle per state
+Chart = pd.df(columns=["state"])
+if st.checkbox('Line/Bar Graph of Chipotle Dataset'):
+    st.subheader('This is a Line and Bar Graph of Amount of Chipotle per State')
+    st.bar_chart(Chart)
+    st.line_chart(Chart)
+
+## code-block
+if st.checkbox('Press if you wish to see a snippet of code used'):
+    code = '''## sorting the dataset by Eastern and Western Americas at the longitude of -97.5
+    df_sorted = df.sort_values(by='longitude', ascending=False)
+    chipotle_east = df_sorted.head(1844)
+    chipotle_west = df_sorted.tail(785)'''
+    st.code(code, language='python')
