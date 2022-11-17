a495f683b3fab1bf7506bcd4ac819dd56ab1a247
Wenjin Kuang <wenkuajin@gmail.com>
2022-11-17 15:42:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+## creating a header for this dashboard
+st.header('Chipotle in America')
+
+## creating some text about the dashboard 
+st.text('If you are craving Chipotle look for a location here!')
+
 ## importing the dataset used for this dashboard
 df = pd.read_csv('chipotle_stores.csv')
 df.columns
@@ -11,9 +17,5 @@ df_sorted = df.sort_values(by='longitude', ascending=False)
 chipotle_east = df_sorted.head(1844)
 chipotle_west = df_sorted.tail(785)
 
-## creating a header for this dashboard
-st.header('Chipotle in America')
-
-## creating some text about the dashboard 
-st.text('If you are craving Chipotle look for a location here!')
-
+df = pd.DataFrame(columns=['latitude', 'longitude'])
+st.map(df)
